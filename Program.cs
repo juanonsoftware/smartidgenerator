4890f5a20f125ce00f29e7d59f1ee53df18a20b7
juanonsoftware <juanonsoftware@github.com>
2016-02-23 16:00:43 +0700

    Testing code of the TimebaseHexIdGenerator

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using GdNet.Common.IdGenerators;
+using System;
+
+namespace UniqueNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new TimebaseHexIdGenerator(1);
+            var t1 = DateTime.Now.Second;
+            var count = 0;
+
+            while (DateTime.Now.Second - t1 < 1)
+            {
+                Console.WriteLine(g.NewId());
+                count += 1;
+            }
+
+            Console.WriteLine("Total: " + count);
+            Console.ReadLine();
+        }
+    }
+}
